e32d64b8681b863bcd674870bcb99981d1b17ad2
Laurie <laurie201@protonmail.ch>
2023-08-20 14:41:43 +0100

    Implemented a Network first strategy for the service worker.

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,32 @@
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches.open('my-cache').then((cache) => {
-        return cache.addAll([
-          'manifest.json',
-          'service-worker.js',
-          'index.html',
-          'styles.css',
-          'app.js',
-          'scripts.js',
-          'images/icon-192x192.png',
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', (event) => {
+  event.waitUntil(
+    caches.open('cache').then((cache) => {
+      return cache.addAll([
+        'manifest.json',
+        'service-worker.js',
+        'index.html',
+        'styles.css',
+        'app.js',
+        'scripts.js',
+        'images/icon-192x192.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method === 'GET') {
     event.respondWith(
-      caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
-      })
+      fetch(event.request)
+        .then(async (response) => {
+          const cache = await caches.open('cache');
+          cache.put(event.request, response.clone());
+          return response;
+        })
+        .catch(async () => {
+          const response = await caches.match(event.request);
+          return response;
+        })
     );
-  });
-  
+  }
+});
